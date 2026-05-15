3ec9752d34ea467e18e538c447f37e82ba59592e
luhverchikv <luhverchikv@icloud.com>
2026-05-15 10:21:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,29 @@
 # config.py
+
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str
+    owner_id: int
+
+
+@dataclass
+class Config:
+    bot: TgBot
+
+
+# Инициализация Env
+env = Env()
+env.read_env()  # Читаем из .env файла в корне проекта
+
+
+# Создаем конфиг
+config = Config(
+    bot=TgBot(
+        token=env('BOT_TOKEN'),
+        owner_id=env.int('OWNER_ID')
+    ),
+)
