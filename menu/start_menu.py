8af5da3030b01e21cc8ab2b440885cbac17210e2
luhverchikv <luhverchikv@icloud.com>
2026-05-15 13:16:58 +0300

    Update start_menu.py

--- menu/start_menu.py
+++ menu/start_menu.py
@@ -1,5 +1,5 @@
 # menu/start_menu.py
-
+import asyncio
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
@@ -27,6 +27,9 @@ start_text = """
 async def command_start(message: Message) -> None:
     await message.delete()
     user_id = message.from_user.id
+    if not await asyncio.to_thread(db.user_exists, user_id):
+        await asyncio.to_thread(db.add_user, user_id)
+
     #if not db.user_exists(user_id):
         #db.add_user(user_id) # внести пользователя в таблицу users
         # если не моздано, то создать директорию, где будут храниться файлы базы знаний
