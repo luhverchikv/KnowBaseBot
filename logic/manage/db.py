838aa97e9ce34a15f82367f10f8ef72085585519
luhverchikv <luhverchikv@icloud.com>
2026-05-17 07:03:20 +0300

    Update db.py

--- logic/manage/db.py
+++ logic/manage/db.py
@@ -201,22 +201,6 @@ class Database:
                 WHERE id=?
             ''', (tokens.prompt_tokens, tokens.completion_tokens, tokens.total_tokens, question_id))
 
-    #def get_token_stats(self, user_id: int, days: int = None) -> dict:
-        #where = f"AND DATE(generated_at) >= DATE('now', '-{days} days')" if days else ""
-        #self.cursor.execute(f'''
-            #SELECT 
-                #SUM(gen_total_tokens) as gen_total,
-                #SUM(eval_total_tokens) as eval_total,
-                #SUM(gen_total_tokens + eval_total_tokens) as grand_total
-            #FROM quiz_questions 
-            #WHERE user_id = ? {where}
-        #''', (user_id,))
-        #row = self.cursor.fetchone()
-        #return {
-            #'generation': row[0] or 0,
-            #'evaluation': row[1] or 0,
-            #'total': row[2] or 0
-        #}
     
     def get_token_stats(self, user_id: Optional[int] = None, days: int = None) -> dict:
         """
@@ -241,3 +225,10 @@ class Database:
             'total': row[2] or 0
         }
     
+    def get_user_max_file_size(self, user_id: int) -> float:
+        """Возвращает максимальный размер файла для пользователя в МБ."""
+        self.cursor.execute("SELECT max_file_size_mb FROM users WHERE user_id = ?", (user_id,))
+        result = self.cursor.fetchone()
+        return result[0] if result else 0.25
+        
+        
