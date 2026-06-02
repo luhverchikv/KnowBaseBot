99687ee92090dcfbc9108d143675fc4a28e90066
luhverchikv <luhverchikv@icloud.com>
2026-06-02 18:48:15 +0300

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -5,11 +5,10 @@ from pathlib import Path
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-# Путь к БД (создаётся в папке database/)
 
 # Асинхронный движок
 engine = create_async_engine(
-    DB_URL, 
+    DB_URL=os.getenv('DB_URL'),
     echo=False,  # Поставьте True для отладки SQL-запросов
     pool_pre_ping=True
 )
