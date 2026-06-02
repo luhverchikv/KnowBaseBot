2cc876da527a51ee9725b382d1cff07f219f119f
luhverchikv <luhverchikv@icloud.com>
2026-06-02 19:16:24 +0300

    Update requests.py

--- database/requests.py
+++ database/requests.py
@@ -3,8 +3,8 @@ from sqlalchemy import select, update, delete
 
 async def set_user (tg_id):
     async with async_session() as session:
-        user = await session. scalar(select (User). where(User.tg_id == tg_id))
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
 
         if not user:
-            session.add (User(tg_id=tg_id))
+            session.add(User(tg_id=tg_id))
             await session.commit()
