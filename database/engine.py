90cc3bbdc03b55242bd4fcda6f70e5b4639707dc
luhverchikv <luhverchikv@icloud.com>
2026-06-02 18:38:14 +0300

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -1,12 +1,11 @@
 # database/engine.py
+import os
 import asyncio
 from pathlib import Path
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 # Путь к БД (создаётся в папке database/)
-DB_PATH = Path(__file__).parent.parent / "database" / "knowbase.db"
-DB_URL = f"sqlite+aiosqlite:///{DB_PATH.resolve()}"
 
 # Асинхронный движок
 engine = create_async_engine(
